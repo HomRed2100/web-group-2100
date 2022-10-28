c345d84aba5c32b9691624c95b9611800b663b17
HomRed2100 <homka2100@yandex.ru>
2022-10-28 13:53:09 +0300

    Вызов подложки через js

--- js/login-popup.js
+++ js/login-popup.js
@@ -1,5 +1,6 @@
 let loginButton = document.querySelector('.login-button');
 let loginPopup = document.querySelector('.login-popup');
+let popupBack = document.querySelector('.page');
 
 loginButton.addEventListener('click', showPopup);
 
@@ -7,4 +8,5 @@ function showPopup(evt){
     console.log(evt);
     evt.preventDefault();
     loginPopup.classList.toggle('show-popup');
+    popupBack.classList.toggle('popup-backdrop');
 }
