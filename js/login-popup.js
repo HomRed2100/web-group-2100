0063bb3ac7a88b6fa98e5128470ca66f586b327a
HomRed2100 <homka2100@yandex.ru>
2022-10-28 14:38:49 +0300

    Добавила функцию для своей страницы

--- js/login-popup.js
+++ js/login-popup.js
@@ -10,3 +10,14 @@ function showPopup(evt){
     loginPopup.classList.toggle('show-popup');
     popupBack.classList.toggle('popup-backdrop');
 }
+
+let choiceButton = document.querySelector('.button');
+let choicePopup = document.querySelector('.choice-popup');
+
+choiceButton.addEventListener('click', showchoicePopup);
+
+function showchoicePopup(evt){
+    console.log(evt);
+    evt.preventDefault();
+    choicePopup.classList.toggle('show-choice-popup');
+}
